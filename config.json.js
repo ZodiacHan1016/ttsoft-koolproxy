e11c14cb36a341f79a2725a47780ad1757ac9fcb
lixin <sadoneli@gmail.com>
2017-05-05 23:22:30 +0800

    first commit

---
+++ config.json.js
@@ -0,0 +1,9 @@
+{
+    "build_date": "2017-05-02_17:24:18", 
+    "description": "去屑不油腻", 
+    "home_url": "Module_koolproxy.asp", 
+    "md5": "e654f751552e62b3a183b48324b7f779", 
+    "module": "koolproxy", 
+    "title": "koolproxy", 
+    "version": "3.4.0"
+}
