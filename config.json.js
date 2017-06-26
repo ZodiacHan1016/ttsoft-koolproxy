6eceb39273f2b268ac26f263afd9e086877e8111
lixin <sadoneli@gmail.com>
2017-06-26 17:34:26 +0800

    update koolproxy

--- config.json.js
+++ config.json.js
@@ -2,8 +2,8 @@
     "build_date": "2017-05-02_17:24:18", 
     "description": "去屑不油腻", 
     "home_url": "Module_koolproxy.asp", 
-    "md5": "b46e69a2c779b9d313ca224ff3349d85", 
+    "md5": "d377a10ea1594b6d291ca25e25d4cac5", 
     "module": "koolproxy", 
     "title": "koolproxy", 
-    "version": "3.4.2"
+    "version": "3.4.3"
 }
